a6f6d08267dedcac7be0afa99218daebb625d29b
Jack <jack.butterfield@live.co.uk>
2020-07-20 12:16:59 +0100

    Completed functionality for text to update as per individuals profile, however additional logic for profile picture required

--- source/Tyro/app/src/main/java/com/example/projecttyro/ProfileActivity.java
+++ source/Tyro/app/src/main/java/com/example/projecttyro/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.projecttyro;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,20 +27,93 @@ public class ProfileActivity extends AppCompatActivity {
         //set that text to display the profile name
         profileName.setText(testingProfile.getName());
 
-        //the above is copied below
+        //the following views are only available if not hidden
         TextView profileLocation = (TextView) findViewById(R.id.profileLocation);
-        profileLocation.setText(testingProfile.getHomeLocation());
+        if (testingProfile.homeLocationIsHidden()){
+            profileLocation.setText(" ");
+        }
+        else
+            profileLocation.setText(testingProfile.getHomeLocation());
 
+        //the above is copied below
         TextView profileJobTitle = (TextView) findViewById(R.id.profileJobTitle);
-        profileJobTitle.setText(testingProfile.getJobTitle());
+        if (testingProfile.jobTitleIsHidden())
+            profileJobTitle.setText(" ");
+        else
+            profileJobTitle.setText(testingProfile.getJobTitle());
 
         TextView profileWorkLocation = (TextView) findViewById(R.id.profileWorkLocation);
-        profileWorkLocation.setText(testingProfile.getWorkLocation());
+        if (testingProfile.workLocationIsHidden())
+            profileWorkLocation.setText(" ");
+        else
+            profileWorkLocation.setText(testingProfile.getWorkLocation());
+
+        TextView interestsTitle = (TextView) findViewById(R.id.interestsTitle);
+        TextView interestsBody = (TextView) findViewById(R.id.interestsBody);
+        if (testingProfile.interestsAreHidden()) {
+            interestsBody.setText(" ");
+            interestsTitle.setText(" ");
+        }
+        //if the user has interests, the first 4 will be displayed (if they have this many) - the
+        //rest will be displayed in a "more" pop up, this will be added later
+        else{
+            //string to store the interests
+            String interestsToShow;
+            //gets the new line operator for the given system
+            String newLine = System.getProperty("line.separator");
+
+            //switch to ensure no array out of bounds if the user has less than 4 interests
+            switch (testingProfile.getInterests().length){
+                case 0:
+                    interestsToShow = "No interests recorded";
+                    break;
+                case 1:
+                    interestsToShow = testingProfile.getInterests()[0] + newLine;
+                    break;
+                case 2:
+                    interestsToShow = testingProfile.getInterests()[0] + newLine
+                            + testingProfile.getInterests()[1] + newLine;
+                    break;
+                case 3:
+                    interestsToShow = testingProfile.getInterests()[0] + newLine
+                            + testingProfile.getInterests()[1] + newLine
+                            + testingProfile.getInterests()[2] + newLine;
+                    break;
+                default:
+                    interestsToShow = testingProfile.getInterests()[0] + newLine
+                            + testingProfile.getInterests()[1] + newLine
+                            + testingProfile.getInterests()[2] + newLine
+                            + testingProfile.getInterests()[3] + newLine;
+                    break;
+            }
+            interestsBody.setText(interestsToShow);
+            interestsTitle.setText("Interests");
+        }
+
+        //logic to display if the user is willing to car share
+        TextView carSharingTitle = (TextView) findViewById(R.id.carSharingTitle);
+        TextView carSharingBody = (TextView) findViewById(R.id.carSharingBody);
+        //hides the possibility if checked
+        if(testingProfile.carSharingIsHidden()){
+            carSharingTitle.setText(" ");
+            carSharingBody.setText(" ");
+        }
+        //if not hidden
+        else{
+            //display title
+            carSharingTitle.setText("Car Sharing");
+            //display yes or no
+            if(testingProfile.isCarSharing())
+                carSharingBody.setText("Yes");
+            else
+                carSharingBody.setText("No");
+        }
 
 
         //functionality to load profile picture
-       // ImageView profilePicture = (ImageView) findViewById(R.id.profilePicture);
-        //profilePicture.set
+        ImageView profilePicture = (ImageView) findViewById(R.id.profilePicture);
+        //will add image file here when one is made ----------------------------------------------
+        profilePicture.setImageBitmap(BitmapFactory.decodeFile("pathToImageFile"));
 
     }
 
